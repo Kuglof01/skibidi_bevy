3b03c3c019322e3a0e0c1900d2c57024db15002d
Oláh Kende <155627299+Kuglof01@users.noreply.github.com>
2025-01-05 15:39:25 +0100

    sigma boy

---
+++ src/main.rs
@@ -0,0 +1,29 @@
+
+use bevy::prelude::*;
+/*
+fn main() {
+    App::new().add_startup_system(setup)
+        .add_system(print_names)
+        .run()
+}
+pub fn setup(mut commands: Commands) {
+    commands.spawn( Person {
+        name: "Alice".to_string(),
+
+    });
+}
+
+pub fn print_names(personquery: Query<&Person>) {
+    for person in personquery.iter() {
+        println!("Name: {}", person.name);
+    }
+}
+#[derive(Component)]
+pub struct Person {
+    pub name: String,
+}
+
+ */
+fn main() {
+
+}
