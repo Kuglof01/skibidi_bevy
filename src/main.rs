a8ed4d6657ecd2d04eff6c8c1f4ae29cb63aabdd
Administrator <olahkende11@gmail.com>
2025-01-22 12:42:18 +0100

    planet fix sigma balls apad fassza gardener

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,69 @@
 use bevy::prelude::*;
 
 fn main() {
-    App::new().add_plugins(DefaultPlugins).add_systems(Startup, (spawn_planet, spawn_light, spawn_camera)).run();
-}
-
-fn spawn_camera(mut commands: Commands) {
-    let camera = Camera3dBundle {
-        transform: Transform::from_xyz(0.0,0.0,15.0),
-        ..default()
-    };
-    commands.spawn(camera);
-}
-fn spawn_light(
-    mut commands: Commands,
-) {
-    let light = PointLightBundle {
-        point_light: PointLight {
-            intensity: 30000.0,
-            ..default()
-        },
-        transform: Transform::from_xyz(-2.0, 2.5, 5.0),
-        ..default()
-    };
-    commands.spawn(light);
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_startup_system(setup)
+        .run();
 }
 
 
-fn spawn_planet(
+fn setup(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
     asset_server: Res<AssetServer>,
-
 ) {
-    let planet_material = materials.add(StandardMaterial {
-        base_color_texture: Some(asset_server.load("sprites/texture.jpg")),
+
+
+    commands.spawn((
+        PbrBundle {
+            mesh: meshes.add(Mesh::from(shape::Cube { size: 100.0 })), // Large cube
+            material: materials.add(StandardMaterial {
+                base_color_texture: Some(asset_server.load("textures/skybox.png")), // Skybox texture
+                unlit: true, // No lighting effects
+                ..default()
+            }),
+            transform: Transform::from_scale(Vec3::new(-1.0, -1.0, -1.0)), // Inverted normals
+            ..default()
+        },
+    ));
+
+    // Camera
+    commands.spawn(Camera3dBundle {
+        transform: Transform::from_xyz(0.0, 0.0, 7.0).looking_at(Vec3::ZERO, Vec3::Y),
         ..default()
     });
 
+    // Light
+    commands.spawn(PointLightBundle {
+        point_light: PointLight {
+            intensity: 9000.0,
+            range: 100.0,
+            ..default()
+        },
+        transform: Transform::from_xyz(-5.0, 4.0, 5.0),
+        ..default()
+    });
+
+    // Sphere with texture
     commands.spawn(PbrBundle {
         mesh: meshes.add(Mesh::from(shape::UVSphere {
             radius: 1.0,
             sectors: 32,
             stacks: 16,
         })),
-        transform: Transform::from_xyz(0.0, 0.0, 0.0),
-        material: planet_material,
+        transform: Transform {
+            translation: Vec3::new(0.0, 1.0, 0.0), // Place at the sphere's surface
+            rotation: Quat::from_rotation_x(-std::f32::consts::FRAC_PI_2), // Align to the sphere's top
+            ..default()
+        },
+        material: materials.add(StandardMaterial {
+            base_color_texture: Some(asset_server.load("sprites/texture.png")),
+            perceptual_roughness: 0.9,
+            metallic: 0.0,
+            ..default()
+        }),
         ..default()
     });
 }
